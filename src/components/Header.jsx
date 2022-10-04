63fba5cce56d04b734c4bbb6d69b455e415fd913
Sultani Sanan <54854030+sultani93@users.noreply.github.com>
2022-10-04 15:22:40 +0400

    anime fetch done

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        The <strong>Anime</strong> Database
+      </h1>
+    </header>
+  )
+}
+
+export default Header
